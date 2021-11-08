c104cd82cd9a7bc9996252f6dca3e40a95bfa478
nivedhasreenivasan <73293294+nivedhasreenivasan@users.noreply.github.com>
2021-11-08 07:50:49 -0600

    Update game.py

--- game.py
+++ game.py
@@ -7,7 +7,22 @@ root.title("Crazy 8")
 c = Canvas(root)
 root.geometry('1500x900')
 c.pack(fill=BOTH, expand=True)
+#Create menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+#Create Options Menu
+options_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label='Options', menu= options_menu)
+options_menu.add_command(label="Reset")
 
-def menu():
+def skip():
+    global skipButton
+    global clicked
+    clicked = True
+    skipButton = Button(root, text="SKIP", font=("Helvetica", 7), height = 3, width = 9, bg="SystemButtonFace", command=lambda: skip_click(skipButton))
+    skipButton.place(x=710,y=650)
+
+
+skip()
 
 root.mainloop()
