0c47bc5d9c37d6ef1302ed65fef48394f3187a55
nivedhasreenivasan <73293294+nivedhasreenivasan@users.noreply.github.com>
2021-11-08 07:30:21 -0600

    Create game.py

---
+++ game.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import csv
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Crazy 8")
+c = Canvas(root)
+root.geometry('1500x900')
+c.pack(fill=BOTH, expand=True)
+
+def menu():
+
+root.mainloop()
