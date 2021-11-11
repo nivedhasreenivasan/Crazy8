ce7ae681e74c691604958d7d3c5105264188e202
Derek Chen <53978637+DereC4@users.noreply.github.com>
2021-11-11 08:03:35 -0600

    hi

--- temp.py
+++ temp.py
@@ -1,85 +1,74 @@
-<<<<<<< HEAD
 import requests, json
 import urllib.request
 from PIL import Image
 
-=======
-from tkinter import *
-import tkinter as tk
-import csv
-from PIL import ImageTk, Image
-import requests
-import urllib.request
-import json
-from io import BytesIO
-
-root = Tk()
-root.title("Crazy 8")
-c = Canvas(root)
-root.geometry('1500x900')
-c.pack(fill=BOTH, expand=True)
-#Create menu
-my_menu = Menu(root)
-root.config(menu=my_menu)
-#Create Options Menu
-options_menu = Menu(my_menu, tearoff=False)
-my_menu.add_cascade(label='Options', menu= options_menu)
-options_menu.add_command(label="Reset")
-player1Count = 0
-player2Count = 0
-playerNum = 1 #default
-w = tk.Label(root, text="Player " + str(playerNum))
-w.pack()
->>>>>>> def5206669879b0c0c25933545ffebb81d7bd391
+deck_id = 0
 
 def start_game():
+    """Makes a new deck, discard\n
+    Adds 7 cards to each player
+    """
     global deck_id
     r = requests.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
     print(r.json())
     temp = r.json()
     deck_id = temp['deck_id']
-    imageArr = []
-    codeArr = []
-    #print(deck_id)
+    # print(deck_id)
+    # Initialize Discard
+    temp_top_card = draw_card()
+    r = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/pile/discard/add/?cards='+temp_top_card)
     # Initialize Both Hands
     for x in range (1,3):
         print('Giving Player '+str(x)+' cards')
         for y in range (1,8):
-    #       Add card to player hand
+        #   Add card to player hand
             temp_drawed_card = draw_card()
-            code = temp_drawed_card[0]['code']
-            image = temp_drawed_card[0]['image']
-            r = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/pile/player'+str(x)+'/add/?cards='+code)
-            imageArr.append(image)
-            codeArr.append(code+".png")
-    for x,y in zip(imageArr, codeArr) :
-        print (x)
-        print(y)
-        urllib.request.urlretrieve(str(x),str(y))
-        img = Image.open(str(y))
-        img.show()
+            r = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/pile/player'+str(x)+'/add/?cards='+temp_drawed_card)
 
-<<<<<<< HEAD
 def check_player1_hand():
+    """Checks the player hand\n
+    Returns the hand codes in an array
+    """
+    temp_array = []
     r = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/pile/player1/list/')
-    print(r.json())
+    for temp in r.json()['piles']['player1']['cards']:
+        temp_array.append(temp['code'])
+    # print(temp_array)
+    return(temp_array)
 
-=======
-def cardClicked():
-    pass
->>>>>>> def5206669879b0c0c25933545ffebb81d7bd391
+def check_player2_hand():
+    """Checks the player hand\n
+    Returns the hand codes in an array
+    """
+    temp_array = []
+    r = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/pile/player2/list/')
+    for temp in r.json()['piles']['player2']['cards']:
+        temp_array.append(temp['code'])
+    # print(temp_array)
+    return(temp_array)
+
 def draw_card():
+    """Draws a card from the deck\n 
+    Returns the card code 
+    """
     # print('https://deckofcardsapi.com/api/deck/'+deck_id+'/draw/?count=1')
     r = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/draw/?count=1')
     # print(r.json())
     drawed_card = r.json()['cards']
-<<<<<<< HEAD
     # print('Drawed Card: ' + str(drawed_card))
     print('Drawed Card: ' + str(drawed_card[0]['code']))
-=======
->>>>>>> def5206669879b0c0c25933545ffebb81d7bd391
     # code = drawed_card[0]['code']
-    return drawed_card
+    return drawed_card[0]['code']
+
+def draw_card_from_player1():
+    r = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/pile/player1/draw/?count=1')
+    drawed_card = r.json()['cards']
+    return drawed_card[0]['code']
+
+def draw_card_from_player2():
+    r = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/pile/player2/draw/?count=1')
+    drawed_card = r.json()['cards']
+    return drawed_card[0]['code']
 
 def add_to_discard(card):
     card_code = card[0]['code']
@@ -92,44 +81,39 @@ def list_played_cards():
 
 def restart_game():
     r = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/pile/discard/return/')
-    print(r.json())
-<<<<<<< HEAD
+    print('Restarted' + r.json())
     
+# def get_card_image_debug():
+#     r = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/pile/player2/draw/?count=7')
+#     drawed_card = r.json()['cards']
+#     for x in drawed_card:
+#         print(x)
+
 def playable(card):
-    card_code = card[0]['code']
-    r = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/pile/discard/return/')
+    """Takes in a card CODE, not the entire json\n
+    Compares this with the top of the discard for the faction and face value\n 
+    Special Cases:\n
+    Jack = 11\n
+    Queen = 12\n
+    King = 13\n
+    Ace = 01\n
+    10 = 0\n
+    """
+    r = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/pile/discard/list/')
     temp_card = r.json()['piles']['discard']['cards'][0]
-    print(temp_card)
-    temp_code = temp_card[0]['code']
-    if(temp_code[1]==card_code[1]):
+    temp_code = temp_card['code']
+    print(temp_code)
+    if(temp_code[1]==card[1]):
         return True
-    if(temp_code[0: 1]==card_code[0: 1]):
+    if(temp_code[0: 1]==card[0: 1]):
         return True
+    return False
 
 start_game()
-# check_player1_hand()
-=======
+print(check_player1_hand())
+tempcard = draw_card_from_player1()
+print(playable(tempcard))
 
-start_game()
-def skip(self):
-    global skipButton
-    global clicked
-    self.clicked = True
-    skipButton = Button(root, text="SKIP", font=("Helvetica", 7), height = 3, width = 9, bg="SystemButtonFace", command=lambda: skip_click(skipButton))
-    skipButton.place(x=710,y=650)
-
-def turn(self): #does not work
-    global player1Turn
-    global player2Turn
-    if (player1Count == player2Count):
-        player1Turn = True
-    elif (player1Count > player2Count):
-        player2Turn = True
-    elif (player1Count < player2Count):
-        player1Turn = True
-    if (self.clicked == True and player1Turn == True):
-        player1Turn == False
-    elif (self.clicked == True and player2Turn == True):
-        player2Turn == False
-root.mainloop()
->>>>>>> def5206669879b0c0c25933545ffebb81d7bd391
+# rtemp = requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/pile/player1/draw/?count=1')
+# print(rtemp.json())
+# print(playable())
